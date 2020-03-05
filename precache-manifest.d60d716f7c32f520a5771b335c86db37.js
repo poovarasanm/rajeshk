afdfe44f9c8907337fbc1ee6d8d8869970f841a0
Coder <poovarasanbe@gmail.com>
2020-03-06 00:30:49 +0530

    Add files via upload

---
+++ precache-manifest.d60d716f7c32f520a5771b335c86db37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadc1906eb864844de212f71d3e5b833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c75ddec5fac718b463",
+    "url": "/static/css/main.6bc6421b.chunk.css"
+  },
+  {
+    "revision": "8e8f7802b6c13fecd396",
+    "url": "/static/js/2.7b478f17.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.7b478f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c75ddec5fac718b463",
+    "url": "/static/js/main.b1c3a935.chunk.js"
+  },
+  {
+    "revision": "d3b70f387e7b4077a17c",
+    "url": "/static/js/runtime-main.1f23d35e.js"
+  }
+]);
